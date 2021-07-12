9f23cd111b514c5251e1bad39eb86c94e5a35642
Saif Ali Shaik <saifshine7@gmail.com>
2021-07-12 20:39:30 +0530

    📦 NEW: class components to build details page

--- src/Pet.js
+++ src/Pet.js
@@ -1,20 +1,22 @@
+import { Link } from "react-router-dom";
 import React from "react";
 
-// const Pet = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h2", {}, props.name),
-//     React.createElement("h3", {}, props.animal),
-//     React.createElement("h3", {}, props.breed),
-//   ]);
-// };
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = `http://pets-images.dev-apis.com/pet/none.jpg`;
+  if (images.length) {
+    hero = images[0];
+  }
 
-const Pet = (props) => {
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <h3>{props.animal}</h3>
-      <h3>{props.breed}</h3>
-    </div>
+    <Link to={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </Link>
   );
 };
 
